35d2342ac126ce2df2eade3fb8f0bf90a6b84e9c
JACKSON-KIM-JAEHO <woghjaeho1@gmail.com>
2025-12-10 14:08:18 +0900

    Add Takeoff for MultiCopter

--- build/px4_practice_py_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jaeho/flight_ws/src/px4_practice_py_pkg/install/px4_practice_py_pkg'
--- setup.py
+++ setup.py
@@ -12,12 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'bt_xml'), glob('bt_xml/*.xml')),
+        
+        # [수정] 런치 파일: glob을 사용하여 launch 디렉토리의 모든 .launch.py 파일을 설치
+        (os.path.join('share', package_name, 'launch'), 
+         glob(os.path.join('launch', '*launch.py'))),
+        
+        # [BT XML] bt_xml 디렉토리의 모든 .xml 파일을 설치
+        (os.path.join('share', package_name, 'bt_xml'), 
+         glob(os.path.join('bt_xml', '*.xml'))),
     ],
     
-
-
-    install_requires=['setuptools', 'py-trees'],
+    install_requires=['setuptools', 'py-trees', 'rclpy', 'mavros_msgs', 'geometry_msgs'], 
+    # [참고] ROS2 메시지 및 rclpy는 빌드 의존성이지만, 설치 의존성에 명시하는 것이 좋음.
+    
     zip_safe=True,
     maintainer='jaeho',
     maintainer_email='jaeho@todo.todo',
@@ -30,10 +37,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            #'bt_runner = px4_practice_py_pkg.main:main',
-            'main = px4_practice_py_pkg.main:main',
+            #'main = px4_practice_py_pkg.main:main',
             'offboardtakeoff= px4_practice_py_pkg.offboardtakeoff:main',
-
         ],
     },
 )
